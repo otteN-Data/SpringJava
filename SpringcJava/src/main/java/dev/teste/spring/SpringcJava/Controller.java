5405587defd2c6abbb2faba99cb49b7fc02fcae8
Helio <nettonuneshelio@gmail.com>
2026-05-12 00:31:23 -0300

    Controller e GetMapping

--- SpringcJava/src/main/java/dev/teste/spring/SpringcJava/Controller.java
+++ SpringcJava/src/main/java/dev/teste/spring/SpringcJava/Controller.java
@@ -1,5 +1,17 @@
 package dev.teste.spring.SpringcJava;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Principais anotações para o Controller
+@RestController
+@RequestMapping
 public class Controller {
 
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "BEM-VINDO";
+    }
 }
--- SpringcJava/src/main/java/dev/teste/spring/SpringcJava/springcJavaApplication.java
+++ SpringcJava/src/main/java/dev/teste/spring/SpringcJava/springcJavaApplication.java
@@ -9,6 +9,5 @@ public class springcJavaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(springcJavaApplication.class, args);
 
-
 	}
 }
